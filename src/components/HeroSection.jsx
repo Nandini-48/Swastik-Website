dc608a7e4a9736d93a1bc682351ece88ecd8a7e4
Nandini Gupta <njkstars1212@gmail.com>
2025-03-18 15:32:58 +0530

    Add files via upload

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,10 +1,8 @@
+import { Link } from "react-router-dom";
 import video1 from "../assets/video1.mp4";
 import video2 from "../assets/video2.mp4";
 
 const HeroSection = () => {
-   
-  
-   
   return (
     <div className="flex flex-col items-center mt-6 lg:mt-20">
       <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
@@ -20,18 +18,19 @@ const HeroSection = () => {
         into immersive reality!
       </p>
       <div className="flex justify-center my-10">
-        <a
-  href="#pricing"
-  className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md"
->
-  Start for free
-</a>
+        <Link
+          to="/pricing"
+          className="bg-gradient-to-r from-orange-500 to-orange-800 py-3 px-4 mx-3 rounded-md"
+        >
+          Start for free
+        </Link>
 
-        
-<a href="/documentation" className="py-3 px-4 mx-3 rounded-md border">
-  Documentation
-</a>
-
+        <Link
+          to="/documentation"
+          className="py-3 px-4 mx-3 rounded-md border"
+        >
+          Documentation
+        </Link>
       </div>
       <div className="flex mt-10 justify-center">
         <video
